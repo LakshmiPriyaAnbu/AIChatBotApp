e1aa591f2a3f1541c633ad072905948d7a43a3c1
LakshmiPriyaAnbu <alakshmipriya@greatergoods.com>
2025-07-23 13:02:48 +0530

    Add initial project files and setup for AIChatBotApp

---
+++ AIChatBotApp/AIChatBotApp/AIChatBotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatBotAppApp.swift
+//  AIChatBotApp
+//
+//  Created by Lakshmi Priya on 23/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatBotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
